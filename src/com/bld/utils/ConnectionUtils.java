a05303ac56bc64295a063838d62fdcb85b4a9e52
qianshui <qianshui_tech@163.com>
2014-08-05 20:32:53 +0800

    update

--- src/com/bld/utils/ConnectionUtils.java
+++ src/com/bld/utils/ConnectionUtils.java
@@ -3,14 +3,14 @@ package com.bld.utils;
 public class ConnectionUtils {
 	
 	private static ConnectionUtils _instence;
-	public String ImageUrl="http://whipsnade.gicp.net:8080/ImgServer/ProductImg/";
-	public String SuggestImageUrl="http://whipsnade.gicp.net:8080/ImgServer/SuggestImg/";
-	public String ServerPath="http://whipsnade.gicp.net:8080/BLDService/";
+//	public String ImageUrl="http://whipsnade.gicp.net:8080/ImgServer/ProductImg/";
+//	public String SuggestImageUrl="http://whipsnade.gicp.net:8080/ImgServer/SuggestImg/";
+//	public String ServerPath="http://whipsnade.gicp.net:8080/BLDService/";
 	
 	
-//	public String ImageUrl="http://192.168.1.11:8090/ImgServer/ProductImg/";
-//	public String SuggestImageUrl="http://192.168.1.11:8090/ImgServer/SuggestImg/";
-//	public String ServerPath="http://192.168.1.11:8090/BLDService/";
+	public String ImageUrl="http://121.40.133.207:8080/ImgServer/ProductImg/";
+	public String SuggestImageUrl="http://121.40.133.207:8080/ImgServer/SuggestImg/";
+	public String ServerPath="http://121.40.133.207:8080/BLDService/";
 	
 //	public String ImageUrl="http://192.168.1.106:8080/ImgServer/ProductImg/";
 //	public String SuggestImageUrl="http://192.168.1.106:8080/ImgServer/SuggestImg/";
